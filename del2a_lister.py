35d4a6b02af2dc98c2c7757b7f17f6161f78e98d
Fredrik277670 <144719800+Fredrik277670@users.noreply.github.com>
2023-11-04 12:06:34 +0100

    Update del2a_lister.py

--- del2a_lister.py
+++ del2a_lister.py
@@ -36,9 +36,50 @@ with open(csv_file, mode = "r", newline = "") as file:
         skydekke.append(i[6])
         vind.append(i[7])
 
-#Tilpasser listen "nedbor" til bruk i oppgave del2f
+#Del2e: Tilpasser listen "temperatur"
+for i in range(1, len(temperatur)):
+    if "," in temperatur[i]:
+        temperatur[i] = nedbor[i].replace(",", ".")
+    if temperatur[i] != "-" and temperatur[i] != "":
+        temperatur[i] = float(temperatur[i])
+
+#Del2f: Tilpasser listen "nedbor"
 for i in range(1, len(nedbor)):
     if "," in nedbor[i]:
         nedbor[i] = nedbor[i].replace(",", ".")
     if nedbor[i] != "-" and nedbor[i] != "":
         nedbor[i] = float(nedbor[i])
+
+
+#Del2e: Legger til temperaturverdier til en temperatur-dictionary.
+temperatur_dict = {}
+for i in range(1, len(tid)):
+    aar = tid[i]
+    if len(aar) > 1 and aar[6:10] not in temperatur_dict:
+        temperatur_dict[aar[6:10]] = []
+    elif aar[6:10] in temperatur_dict:
+        temperatur_dict[aar[6:10]].append(temperatur[i])
+
+#Del2e: Legger til år med over 300 målinger i ny dictionary.
+ny_temperatur_dict = {}
+for i in temperatur_dict:
+    length = len(temperatur_dict[i])
+    if length > 300:
+        ny_temperatur_dict[i] = temperatur_dict[i]
+
+
+#Del2f: Legger til nedbørsverdier til en nedbørs-dictionary.
+nedbor_dict = {}
+for i in range(1, len(tid)):
+    aar = tid[i]
+    if len(aar) > 1 and aar[6:10] not in nedbor_dict:
+        nedbor_dict[aar[6:10]] = []
+    elif aar[6:10] in nedbor_dict:
+        nedbor_dict[aar[6:10]].append(nedbor[i])
+
+#Del2f: Legger til år med over 300 målinger i ny dictionary.
+ny_nedbor_dict = {}
+for i in nedbor_dict:
+    length = len(nedbor_dict[i])
+    if length > 300:
+        ny_nedbor_dict[i] = nedbor_dict[i]
