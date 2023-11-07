3e344151ad209bb2b8a6ba976985a1ffd1c69207
Mancorl <133156710+Mancorl@users.noreply.github.com>
2023-11-07 09:25:34 +0100

    Add files via upload

---
+++ del1d_returner_liste_av_storre_tall.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 15 10:08:35 2023
+
+@author: audun
+"""
+
+def differanse(listen, tall):
+    nyliste = []
+    for i in range(0, len(listen)):
+        x = int(listen[i]) - tall
+        if x > 0:
+            nyliste.append(x)
+    return nyliste
