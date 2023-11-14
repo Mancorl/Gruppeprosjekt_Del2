d65dc3c2c695c69cc6bb39978b85d8a60c3ab8b7
Fredrik277670 <144719800+Fredrik277670@users.noreply.github.com>
2023-11-14 16:12:01 +0100

    Update del2i_temperatur_gjennomsnitt_vB.py

--- del2i_temperatur_gjennomsnitt_vB.py
+++ del2i_temperatur_gjennomsnitt_vB.py
@@ -1,11 +1,11 @@
 """
-En måte å sjekke trender i temperatur gjennom året er å ta gjennomsnittet av temperaturen
-for en tidsperiode (for eksempel ei uke eller en måned) og så sjekke om snittet endrer seg.
-Ved å ta gjennomsnittet så fjerner man at temperaturen går opp og ned fra en dag til den
-neste. Regn ut gjennomsnittstemperaturen for hver måned (for eksempel april 2007) og legg
-disse gjennomsnittene i ei ny liste. Bruk funksjonen fra del 1 deloppgave e) for å regne ut ei
-liste med differanser. Plott både lista over gjennomsnittstemperaturer og lista over
-differanser med måned og år på x-aksen.
+En m책te 책 sjekke trender i temperatur gjennom 책ret er 책 ta gjennomsnittet av temperaturen
+for en tidsperiode (for eksempel ei uke eller en m책ned) og s책 sjekke om snittet endrer seg.
+Ved 책 ta gjennomsnittet s책 fjerner man at temperaturen g책r opp og ned fra en dag til den
+neste. Regn ut gjennomsnittstemperaturen for hver m책ned (for eksempel april 2007) og legg
+disse gjennomsnittene i ei ny liste. Bruk funksjonen fra del 1 deloppgave e) for 책 regne ut ei
+liste med differanser. Plott b책de lista over gjennomsnittstemperaturer og lista over
+differanser med m책ned og 책r p책 x-aksen.
 """
 
 from del1e_differanse import finn_differanse
@@ -14,7 +14,7 @@ from del2a_lister import tid
 import matplotlib.pyplot as plt
 
 #Legger til temperaturverdier til en temperatur-dictionary.
-#Lager også gj_temperatur_dict for å legge til gjennomsnittsverdi per måned.
+#Lager ogs책 gj_temperatur_dict for 책 legge til gjennomsnittsverdi per m책ned.
 maaneder = []
 temperatur_dict = {}
 gj_temperatur_dict = {}
@@ -24,14 +24,14 @@ for i in range(1, len(tid)):
         maaneder.append(maaned[3:10])
         temperatur_dict[maaned[3:10]] = []
         gj_temperatur_dict[maaned[3:10]] = 0
-    elif maaned[3:10] in temperatur_dict and temperatur[i] != "":
+    elif maaned[3:10] in temperatur_dict and temperatur[i] != "-" and temperatur[i] != "":
         temperatur_dict[maaned[3:10]].append(temperatur[i])
 
 #Legger til verdier til gj_temperatur_dict
 for i in temperatur_dict:
     if len(temperatur_dict[i]) != 0:
         gj_snitt = round(sum(temperatur_dict[i]) / len(temperatur_dict[i]))
-    gj_temperatur_dict[i] = gj_snitt
+        gj_temperatur_dict[i] = gj_snitt
 
 #Oppretter liste med gjennomsnittsverdiene for seg selv
 gj_temperatur = []
@@ -41,8 +41,10 @@ for i in gj_temperatur_dict:
 #Oppretter liste med temperaturdifferanse ved bruk av finn_differanse funksjonen
 diff_temperatur = finn_differanse(gj_temperatur)
 
-#Legger til verdi til differanse-liste, slik at den samsvarer med måneds-liste
-diff_temperatur.append(0)
+#Lager egen liste for maneder, som starter på andre måned for differanse
+diff_maaneder = []
+for i in range(1, len(maaneder)):
+    diff_maaneder.append(maaneder[i])
 
 """
 #Differanse uten bruk av funksjon:
@@ -57,7 +59,7 @@ plt.ylabel("temperatur")
 plt.xlabel("Dato")
 plt.show()
 
-plt.plot(maaneder, diff_temperatur, label="Gjennomsnittsdifferanse")
+plt.plot(diff_maaneder, diff_temperatur, label="Gjennomsnittsdifferanse")
 plt.ylabel("temperatur")
 plt.xlabel("Dato")
 plt.show()
